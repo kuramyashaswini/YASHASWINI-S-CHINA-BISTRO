66589e38fb20c472cc226d21c393c9a35696df8b
kuramyashaswini <146028749+kuramyashaswini@users.noreply.github.com>
2024-03-20 22:35:54 +0530

    Add files via upload

---
+++ src/public/menu-item/menu-item.component.js
@@ -0,0 +1,34 @@
+// (function () {
+// "use strict";
+
+// angular.module('public')
+// .component('menuItem', {
+//   templateUrl: 'src/public/menu-item/menu-item.html',
+//   bindings: {
+//     menuItem: '<'
+//   },
+//   controller: MenuItemController
+// });
+
+
+// MenuItemController.$inject = ['ApiPath'];
+// function MenuItemController(ApiPath) {
+//   var $ctrl = this;
+//   $ctrl.basePath = ApiPath;
+// }
+
+// })();
+(function () {
+  "use strict";
+  
+  angular.module('public')
+  .component('menuItem', {
+    templateUrl: 'src/public/menu-item/menu-item.html',
+    bindings: {
+      menuItem: '<',
+      categoryShortName: '<'
+    }
+  });
+  
+  })();
+  
---
+++ src/public/menu-items/menu-items.controller.js
@@ -0,0 +1,28 @@
+// (function () {
+// "use strict";
+
+// angular.module('public')
+// .controller('MenuItemsController', MenuItemsController);
+
+// MenuItemsController.$inject = ['menuItems'];
+// function MenuItemsController(menuItems) {
+//   var $ctrl = this;
+//   $ctrl.menuItems = menuItems;
+// }
+
+// })();
+
+(function () {
+  "use strict";
+  
+  angular.module('public')
+  .controller('MenuItemsController', MenuItemsController);
+  
+  MenuItemsController.$inject = ['menuItems'];
+  function MenuItemsController(menuItems) {
+    var $ctrl = this;
+    $ctrl.menuItems = menuItems;
+  }
+  
+  })();
+  
